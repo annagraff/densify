93f437d80b483b3e95732937a8fb2a38e035d6ab
Marc Lischka <malisc@olive.math.uzh.ch>
2023-08-09 13:27:16 +0200

    More for poster.

--- R/plots-for-poster.R
+++ R/plots-for-poster.R
@@ -1,18 +1,30 @@
-install.packages("ggplot2")  # Install the package if you haven't
-library(ggplot2)  # Load the package
+# Install and load necessary packages
+if (!requireNamespace("ggplot2", quietly = TRUE)) {
+  install.packages("ggplot2")
+}
+library(ggplot2)
+library(RColorBrewer)
+
+# Get ColorBrewer Set1 colors
+set1_colors <- brewer.pal(9, "Set1")
 
 # Sorted row means plot
 sorted_row_means <- sort(rowMeans(full_matrix), decreasing = TRUE)
 row_means_df <- data.frame(Mean = sorted_row_means)
-ggplot(row_means_df, aes(x = reorder(Mean, Mean), y = Mean)) +
-  geom_bar(stat = "identity", fill = "dodgerblue") +
-  labs(title = "Sorted Row Means", x = "Row", y = "Mean") +
-  theme_minimal()
+row_means_df$Row <- seq_len(nrow(row_means_df))
 
-# Sorted column means plot
-sorted_col_means <- sort(colMeans(full_matrix), decreasing = TRUE)
-col_means_df <- data.frame(Mean = sorted_col_means)
-ggplot(col_means_df, aes(x = reorder(Mean, Mean), y = Mean)) +
-  geom_bar(stat = "identity", fill = "dodgerblue") +
-  labs(title = "Sorted Column Means", x = "Column", y = "Mean") +
-  theme_minimal()
+ggplot(row_means_df, aes(x = Row, y = Mean)) +
+  geom_point(color = set1_colors[1], size = 1) +
+  labs(title = "Sorted Row Densities", x = "Row", y = "Density") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+# Sorted column densities plot with a different color
+sorted_col_densities <- sort(colMeans(full_matrix), decreasing = TRUE)
+col_densities_df <- data.frame(Density = sorted_col_densities)
+
+ggplot(col_densities_df, aes(x = seq_along(Density), y = Density)) +
+  geom_point(color = set1_colors[2], size = 1) +
+  labs(title = "Sorted Column Densities", x = "Column", y = "Density") +
+  theme_minimal() +
+  theme(axis.text.x = element_blank())
