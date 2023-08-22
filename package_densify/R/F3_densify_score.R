8fe9bc06e436b392bf8abc92a1572464699062c1
Anna Graff <anna.graff@gmx.ch>
2023-08-22 10:27:45 +0200

    Update F3 to include lowest variable coding exponent, and making  lowest variable coding exponent and  lowest taxon coding exponent optional

--- package_densify/R/F3_densify_score.R
+++ package_densify/R/F3_densify_score.R
@@ -14,7 +14,8 @@
 #'
 #' @param exponent_prop_coded_data Exponent for the proportion of coded data. Default is 1.
 #' @param exponent_available_data_points Exponent for the number of available data points. Default is 1.
-#' @param exponent_lowest_taxon_coding_score Exponent for the lowest taxon score. Default is 1.
+#' @param exponent_lowest_taxon_coding_score Exponent for the lowest taxon score. This exponent is not mandatory, so there is no default.
+#' @param exponent_lowest_variable_coding_score Exponent for the lowest variable score. This exponent is not mandatory, so there is no default.
 #' @param exponent_taxonomic_diversity Exponent for the taxonomic diversity index. This exponent is not mandatory, so there is no default.
 #'
 #' @return The optimal number of iterations with the highest quality score.
@@ -25,14 +26,25 @@
 #'
 #' @export
 
-densify_score <- function(documentation, exponent_prop_coded_data = 1, exponent_available_data_points = 1, exponent_lowest_taxon_coding_score = 1, exponent_taxonomic_diversity = NULL) {
+densify_score <- function(documentation, exponent_prop_coded_data = 1, exponent_available_data_points = 1, exponent_lowest_taxon_coding_score = NULL, exponent_lowest_variable_coding_score = NULL, exponent_taxonomic_diversity = NULL) {
   # Validate input parameters:
   if (!is.numeric(exponent_prop_coded_data) ||
-      !is.numeric(exponent_available_data_points) ||
-      !is.numeric(exponent_lowest_taxon_coding_score)) {
+      !is.numeric(exponent_available_data_points)) {
     stop("Exponents must be numeric.")
   }
+  
+  if (!is.null(exponent_lowest_taxon_coding_score)){
+    if(!is.numeric(exponent_lowest_taxon_coding_score)){
+      stop("Exponents must be numeric.")
+    }
+  }
 
+  if (!is.null(exponent_lowest_variable_coding_score)){
+    if(!is.numeric(exponent_lowest_variable_coding_score)){
+      stop("Exponents must be numeric.")
+    }
+  }
+  
   if(!is.null(exponent_taxonomic_diversity) & ncol(documentation)!=13){
     stop("densify_steps() was carried out without providing a taxonomy matrix, so there cannot be an exponent for taxonomic diversity.")
   }  
@@ -41,10 +53,28 @@ densify_score <- function(documentation, exponent_prop_coded_data = 1, exponent_
     warning("Attention, an exponent for taxonomic diversity would be applicable but was not set, and no default exponent applies.\n")
   }
   
+  if (is.null(exponent_lowest_taxon_coding_score)){
+    warning("Attention, an exponent for lowest taxon coding score was not set, and no default exponent applies.\n")
+  }
+  
+  if (is.null(exponent_lowest_variable_coding_score)){
+    warning("Attention, an exponent for lowest variable coding score was not set, and no default exponent applies.\n")
+  }
+  
+  
   # Calculate the quality score for each iteration:
   quality <- as.numeric(documentation$prop_coded_data)^exponent_prop_coded_data *
-    as.numeric(documentation$available_data_points)^exponent_available_data_points *
-    as.numeric(documentation$worst_tax_abs_coding_density)^exponent_lowest_taxon_coding_score
+    as.numeric(documentation$available_data_points)^exponent_available_data_points
+
+  # if exponent_taxonomic_diversity is provided, include this in the quality score
+  if(!is.null(exponent_taxonomic_diversity)){
+    quality <- (quality * as.numeric(documentation$worst_tax_abs_coding_density)^exponent_lowest_taxon_coding_score)
+  }
+  
+  # if exponent_lowest_variable_coding_score is provided, include this in the quality score
+  if(!is.null(exponent_taxonomic_diversity)){
+    quality <- (quality * as.numeric(documentation$worst_var_abs_coding_density)^exponent_lowest_variable_coding_score)
+  }
   
   # if exponent_taxonomic_diversity is provided, include this in the quality score
   if(!is.null(exponent_taxonomic_diversity)){
