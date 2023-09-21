56928e4854ecfd0aac2961d26da3d3278b015003
Anna Graff <annagraff.zh@gmail.com>
2023-09-21 16:02:09 +0200

    densify_score adaptations: implement generalization regarding number of columns and add "plot" argument to silence plot in default setting

--- R/densify_score.R
+++ R/densify_score.R
@@ -15,10 +15,16 @@
 #' @param documentation A data frame containing the output documentation from the `densify_steps` function.
 #'
 #' @param exponent_prop_coded_data Exponent for the proportion of coded data. Default is 1.
+#'
 #' @param exponent_available_data_points Exponent for the number of available data points. Default is 1.
+#'
 #' @param exponent_lowest_taxon_coding_score Exponent for the lowest taxon score. This exponent is not mandatory, so there is no default.
+#'
 #' @param exponent_lowest_variable_coding_score Exponent for the lowest variable score. This exponent is not mandatory, so there is no default.
+#'
 #' @param exponent_taxonomic_diversity Exponent for the taxonomic diversity index. This exponent is not mandatory, so there is no default.
+#'
+#' @param plot Logical denoting whether the quality scores should be plotted. Default is `FALSE`.
 #'
 #' @return The optimal number of iterations with the highest quality score.
 #'
@@ -29,7 +35,7 @@
 #' @importFrom stats na.omit
 #' @export
 
-densify_score <- function(documentation, exponent_prop_coded_data = 1, exponent_available_data_points = 1, exponent_lowest_taxon_coding_score = NULL, exponent_lowest_variable_coding_score = NULL, exponent_taxonomic_diversity = NULL) {
+densify_score <- function(documentation, exponent_prop_coded_data = 1, exponent_available_data_points = 1, exponent_lowest_taxon_coding_score = NULL, exponent_lowest_variable_coding_score = NULL, exponent_taxonomic_diversity = NULL, plot = FALSE) {
   # Validate input parameters:
   if (!is.numeric(exponent_prop_coded_data) ||
       !is.numeric(exponent_available_data_points)) {
@@ -48,11 +54,11 @@ densify_score <- function(documentation, exponent_prop_coded_data = 1, exponent_
     }
   }
 
-  if(!is.null(exponent_taxonomic_diversity) & ncol(documentation)!=13){
+  if(!is.null(exponent_taxonomic_diversity) & "taxonomic_index" %in% names(documentation)){
     stop("densify_steps() was carried out without providing a taxonomy matrix, so there cannot be an exponent for taxonomic diversity.")
   }
 
-  if (is.null(exponent_taxonomic_diversity) & ncol(documentation)==13){
+  if (is.null(exponent_taxonomic_diversity) & "taxonomic_index" %in% names(documentation)){
     warning("Attention, an exponent for taxonomic diversity would be applicable but was not set, and no default exponent applies.\n")
   }
 
@@ -85,7 +91,9 @@ densify_score <- function(documentation, exponent_prop_coded_data = 1, exponent_
   }
 
   # Plot the quality scores:
-  plot(quality, xlab = "Iteration", ylab = "Quality Score", main = "Quality Score at Each Iteration")
+  if(plot == TRUE){
+    plot(quality, xlab = "Iteration", ylab = "Quality Score", main = "Quality Score at Each Iteration")
+  }
 
   # Identify the first iteration number with the highest quality score:
   optimum <- min(which(quality == max(na.omit(quality))))
