2b084fb5cb54ad74b2e6438e6f2837d76c68c389
marclischka <145458000+marclischka@users.noreply.github.com>
2023-10-23 19:55:39 +0200

    tests 3 and 4
    tests 3 and 4

--- tests/test-densify_score.R
+++ tests/test-densify_score.R
@@ -1,1 +1,30 @@
+# Not sure we need the sample data in the end, but maybe it makes things easier.
+
+context("Test densify_score function with unexpected numerical inputs")
+
+test_that("It handles unexpected numerical inputs", {
+  # Create sample data for testing
+  iteration_log <- data.frame(
+    prop_coded_data = c(0.5, 0.6, 0.7, 0.8),
+    available_data_points = c(400, 300, 200, 100),  # Decreasing values
+    worst_tax_abs_coding_density = c(0.1, 0.2, 0.3, 0.4),
+    worst_var_abs_coding_density = c(0.2, 0.3, 0.4, 0.5),
+    taxonomic_index = c(40, 30, 20, 10)  # Decreasing values
+  )
+
+  # Test the densify_score function with non-numeric exponents
+  expect_error(densify_score(iteration_log, exponent_prop_coded_data = "invalid"))
+
+  # Test the densify_score function with non-numeric exponents_lowest_taxon_coding_score
+  expect_error(densify_score(iteration_log, exponent_lowest_taxon_coding_score = "invalid"))
+
+  # Test the densify_score function with non-numeric exponents_lowest_variable_coding_score
+  expect_error(densify_score(iteration_log, exponent_lowest_variable_coding_score = "invalid"))
+
+  # Test the densify_score function with non-numeric exponents_taxonomic_diversity
+  expect_error(densify_score(iteration_log, exponent_taxonomic_diversity = "invalid"))
+
+  # Add more test cases as needed for other unexpected numerical inputs
+
+})
 
--- tests/test-densify_steps.R
+++ tests/test-densify_steps.R
@@ -1,1 +1,43 @@
+# Not sure we need the sample data in the end, but maybe it makes things easier.
+
+context("Test densify_steps function with incorrect input values")
+
+test_that("It handles incorrect input values", {
+  # Create sample data for testing
+  original_data <- data.frame(
+    Taxon1 = c(1, 2, 3, NA),
+    Taxon2 = c(0, 1, 1, 0),
+    Taxon3 = c(1, 0, 1, 1),
+    Taxon4 = c(0, 0, 0, 0)
+  )
+
+  taxonomy_matrix <- data.frame(
+    id = 1:4,
+    level1 = c("FamilyA", "FamilyB", "FamilyA", "FamilyC")
+  )
+
+  # Test the densify_steps function with negative max_steps
+  expect_error(densify_steps(original_data, max_steps = -1))
+
+  # Test the densify_steps function with zero max_steps
+  expect_error(densify_steps(original_data, max_steps = 0))
+
+  # Test the densify_steps function with non-integer max_steps
+  expect_error(densify_steps(original_data, max_steps = 2.5))
+
+  # Test the densify_steps function with non-integer variability_threshold
+  expect_error(densify_steps(original_data, variability_threshold = 2.5))
+
+  # Test the densify_steps function with mean_type not in the list
+  expect_error(densify_steps(original_data, mean_type = "median"))
+
+  # Test the densify_steps function with taxonomy_weight out of range
+  expect_error(densify_steps(original_data, taxonomy = TRUE, taxonomy_matrix = taxonomy_matrix, taxonomy_weight = 1.5))
+
+  # Test the densify_steps function with coding_weight out of range
+  expect_error(densify_steps(original_data, coding_weight = -0.5))
+
+  # Add more test cases as needed for other incorrect input values
+
+})
 
