99c830b228782d2b3f54209a42bfa6bacfc25849
marclischka <145458000+marclischka@users.noreply.github.com>
2023-10-23 19:42:14 +0200

    Update test-build_flat_taxonomy_matrix.R
    tests

--- tests/test-build_flat_taxonomy_matrix.R
+++ tests/test-build_flat_taxonomy_matrix.R
@@ -1,10 +1,10 @@
-# Disclaimer: This is a first version of a test case generated with ChatGPT to help Marc learn the syntax. ChatGPT has limitations in accurately understanding" complex formats and dimensions, especially in real-world scenarios.
+# Disclaimer: This is a second version of a test case generated with ChatGPT to help Marc learn the syntax. ChatGPT has limitations in accurately understanding" complex formats and dimensions, especially in real-world scenarios.
 
 
 context("Test build_flat_taxonomy_matrix function")
 
-test_that("It correctly flattens a taxonomy", {
-  # Create sample data for testing
+test_that("It correctly flattens a taxonomy and controls arguments", {
+  # Test case with valid input data
   id <- c("A", "B", "C", "D", "E", "F")
   parent_id <- c(NA, "A", "B", "B", "A", "F")
 
@@ -17,12 +17,26 @@ test_that("It correctly flattens a taxonomy", {
   # Check if the number of columns is correct (number of levels in the taxonomy)
   expect_equal(ncol(flat_taxonomy), 3)  # Adjust the expected value as needed
 
-  # Check if the levels are correctly filled
-  expect_equal(flat_taxonomy$level1, c("A", "B", "B", "A", "F"))
-  expect_equal(flat_taxonomy$level2, c("B", "A", "F", NA, NA))
-  expect_equal(flat_taxonomy$level3, c("C", "E", NA, NA, NA))
+  # Test cases with empty id and parent_id vectors
+  empty_id <- character(0)
+  empty_parent_id <- character(0)
 
-  # Add more checks as needed based on your specific expectations
+  empty_taxonomy <- build_flat_taxonomy_matrix(empty_id, empty_parent_id)
+
+  # Check if the result is a data frame
+  expect_is(empty_taxonomy, "data.frame")
+
+  # Check if the data frame is empty
+  expect_true(is.empty(empty_taxonomy))
+
+  # Test cases with id and parent_id of different sizes
+  id_mismatched <- c("A", "B", "C", "D")
+  parent_id_mismatched <- c(NA, "A", "B")
+
+  expect_error(build_flat_taxonomy_matrix(id_mismatched, parent_id_mismatched))
+
+  # Add more test cases as needed to cover out-of-bounds numbers and other scenarios
 
 })
+
 
