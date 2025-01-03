2be3086d0dd5d96d29d5ad3b2bc9bd47264d3b6a
Taras Zakharko <>
2025-01-03 19:55:35 +0100

    Import `glottolog-languoids` directly from CLDF dataset; add an auxiliary script performing this task; clarify the documentation of the dataset

--- R/glottolog_languoids.R
+++ R/glottolog_languoids.R
@@ -1,7 +1,18 @@
-#' Glottolog languoid taxonomy, Version 5.0
+#' Glottolog Languoid Taxonomy, Version 5.0
 #'
-#' This dataset is the languoid.csv file from Glottolog, Version 5.0. It can be used as `taxonomy` argument for the
-#' [densify] function if your dataset uses glottocodes and you want to perform taxonomy-aware data pruning.
+#' Glottolog Languoid Taxonomy, Version 5.0
+#'
+#' A data frame containing linguistic taxonomic information from Glottolog version 5.0.
+#' You can use this dataset as the `taxonomy` argument for the [densify()] function if your
+#' data relies on glottocodes and you wish to perform taxonomy-aware data pruning.
+#'
+#' The dataset was extracted from the official Glottolog CLDF release and converted into a
+#' wide tabular representation. Each row describes a single variety (Glottolog languoid).
+#' The columns `id`, `parent_id`, and `family_id` correspond to the Glottolog code of the
+#' languoid, its parent, and its root node, respectively. The `level` column denotes the
+#' languoid classification (e.g., dialect, language, or family). Singletons and top-level
+#' families have `parent_id` set to `NA`.
+#'
 #'
 #' @name glottolog_languoids
 #' @docType data
@@ -11,7 +22,6 @@
 #' Glottolog 5.0.
 #' Leipzig: Max Planck Institute for Evolutionary Anthropology.
 #' https://doi.org/10.5281/zenodo.10804357
-#' (Available online at http://glottolog.org, Accessed on 2024-03-13.)
+#' (Available online at http://glottolog.org, Accessed on 2023-03-13.)
 #'
 NULL
-
